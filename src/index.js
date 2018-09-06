afc6a26ae3e311483ab4437d75af28b6eac0516f
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-06 06:24:36 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -10,30 +10,52 @@ const BUTCHER_PRODUCTS = [
 
 // Define these exported classes
 export class OlderCoaster extends React.Component {
-  // your code here
-    render() {
-      return (
+  render() {
+    return (
       <div className="oldercoaster">
-   <p>Two grannies having the time of their life!</p>
-   <p>Passengers:</p>
-   <ul>
-     <li>Agnes</li>
-     <li>Muriel</li>
-   </ul>
- </div>
- )
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
   }
 }
 
 export class InFrontOfYou extends React.Component {
-  // your code here
+  render() {
+    return (
+      <div>
+        <p>You shouldn't look too far.</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
-
 export class ButcherShop extends React.Component {
-  // your code here
+  render() {
+    const products = BUTCHER_PRODUCTS.map((p, idx) => (<li key={idx}>{p}</li>))
+    return (
+      <div className="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          { products }
+        </ul>
+      </div>
+    )
+  }
 }
-
-
-ReactDOM.render(<App />,
-  document.getElementById('root')
-)
+export class App extends React.Component {
+  render() {
+    return (
+      <div id="app">
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
