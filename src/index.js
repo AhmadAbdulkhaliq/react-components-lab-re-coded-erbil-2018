143e554b91475f8aa28249492eb984e1601aff43
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-06 06:18:08 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ const BUTCHER_PRODUCTS = [
 // Define these exported classes
 export class OlderCoaster extends React.Component {
   // your code here
-  
+  render(
+    <div class="oldercoaster">
+  <p>Two grannies having the time of their life!</p>
+  <p>Passengers:</p>
+  <ul>
+    <li>Agnes</li>
+    <li>Muriel</li>
+  </ul>
+</div>
+    )
 }
 
 export class InFrontOfYou extends React.Component {
